37f828b7900ab5d2c44d4a1a729eaed22198dca6
Fabrizzio De Bracamonte <fabrizziodbc@gmail.com>
2021-11-03 14:39:22 -0500

    apply dbUrl

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -2,17 +2,18 @@ const { Sequelize } = require('sequelize');
 const { config } = require('../config/config');
 const setupModels = require('../db/models');
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const options = {
+  dialect: 'postgres',
+  logging: config.isProd ? false : true,
+};
 
-const sequelize = new Sequelize(URI, {
-  dialect: "postgres",
-  logging: true,
-})
+if (config.isProd) {
+  options.dialectOptions = { ssl: { rejectUnauthorized: false } };
+}
+
+const sequelize = new Sequelize(config.dbUrl, options);
 setupModels(sequelize);
 
 /* sequelize.sync(); */
 
-module.exports= sequelize;
-
+module.exports = sequelize;
